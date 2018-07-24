b010275cd832154bdb042de9d9460a884125ccc5
Roman Trampush <roman.trampush@se.abb.com>
2018-07-24 16:06:38 +0200

    Initial commit

---
+++ DatingApp.SPA/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpModule,
+      HttpClientModule
+   ],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
